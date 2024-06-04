b843dbcc1bb878dded91cfe291f6f2c4bf981e0a
ImJustLucas <lucas.bellier@icloud.com>
2024-06-04 22:10:44 +0200

    🚧 add rider module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RidersModule } from './riders/riders.module';
 
 @Module({
-  imports: [],
+  imports: [RidersModule],
   controllers: [AppController],
   providers: [AppService],
 })
