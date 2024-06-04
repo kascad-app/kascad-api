404c6a8dd44c49dc46d5d0bd06f0c3c9c5a57958
ImJustLucas <lucas.bellier@icloud.com>
2024-06-04 22:20:41 +0200

    chore: Update npm dependencies and add MongoDB configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RidersModule } from './riders/riders.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongoDBConfigService } from './config/database/mongodb.config';
 
 @Module({
-  imports: [RidersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env['NODE_ENV'] === 'production'
+          ? ['.env.production', '.env']
+          : [
+              '.env.development.local',
+              '.env.local',
+              '.env.development',
+              '.env',
+            ],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongoDBConfigService,
+      inject: [ConfigService],
+    }),
+    RidersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database/mongodb.config.ts
@@ -0,0 +1,52 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  MongooseModuleOptions,
+  MongooseOptionsFactory,
+} from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class MongoDBConfigService implements MongooseOptionsFactory {
+  private readonly logger = new Logger(MongoDBConfigService.name);
+
+  constructor(private readonly _configService: ConfigService) {}
+
+  createMongooseOptions(): MongooseModuleOptions {
+    const dsn: string = this.buildDsn(
+      this._configService.get('MONGODB_METHOD'),
+      this._configService.get('MONGODB_USERNAME'),
+      this._configService.get('MONGODB_PASSWORD'),
+      this._configService.get('MONGODB_SERVER_URI'),
+      this._configService.get('MONGODB_DATABASE'),
+      this._configService.get('MONGODB_PARAMS') || '',
+    );
+    this.logger.debug(`Generated DSN : ${dsn}`);
+
+    return {
+      uri: dsn,
+      connectionFactory: async (connection: Connection) => {
+        return connection;
+      },
+    };
+  }
+
+  private buildDsn(
+    method: string,
+    username: string,
+    password: string,
+    serverUri: string,
+    databaseName: string,
+    params: string,
+  ): string {
+    const paramsArray = params.split(',');
+    params =
+      paramsArray && paramsArray.length > 0 ? params.split(',').toString() : '';
+
+    if (method.includes('srv')) {
+      return `${method}://${username}:${password}@${serverUri}/${databaseName}${params}`;
+    }
+
+    return `${method}://${serverUri}/${databaseName}${params}`;
+  }
+}
