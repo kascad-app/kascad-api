3ed3e72703f4788708bb29ef0dc2c1ee9c62da21
ImJustLucas <lucas.bellier@icloud.com>
2024-06-04 23:18:08 +0200

    🔧 add MongoDBConnection enum for MongoDB connection names

---
+++ src/common/constants/mongoDbConnections.ts
@@ -0,0 +1,6 @@
+enum MongoDBConnection {
+  AUTH = 'auth',
+  RIDERS = 'riders',
+}
+
+export default MongoDBConnection;
--- src/riders/riders.module.ts
+++ src/riders/riders.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { RidersController } from './riders.controller';
 import { RidersService } from './riders.service';
+import { RiderSchema } from './schemas/rider.schema';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoDBConfigService } from 'src/config/database/mongodb.config';
+import MongoDBConnection from 'src/common/constants/mongoDbConnections';
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useClass: MongoDBConfigService,
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      {
+        name: 'Rider',
+        schema: RiderSchema,
+        collection: MongoDBConnection.RIDERS,
+      },
+    ]),
+  ],
   controllers: [RidersController],
   providers: [RidersService],
 })
