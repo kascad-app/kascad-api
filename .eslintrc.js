42c5192bbb49b1e7bf2bf14ca0b254e8b63a9d21
ImJustLucas <lucas.bellier@icloud.com>
2024-06-06 23:06:47 +0200

    ➕ add eslint prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,80 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
-    node: true,
-    jest: true,
+    browser: true,
+    es2021: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  extends: [
+    "next",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier", "simple-import-sort"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: false,
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          /* Style imports */
+          ["^.+\\.?(css)$"],
+
+          /*  imports */
+          ["^nestjs", "^@nestjs"],
+
+          /* Imports starting with @ */
+          ["^@"],
+
+          /* Custom imports */
+          [
+            "^@components",
+            "^@constants",
+            "^@contexts",
+            "^@hooks",
+            "^@misc",
+            "^@pages",
+            "^@plugins",
+            "^@public",
+            "^@services",
+            "^@stores",
+            "^@typesDef",
+            "^@utils",
+          ],
+
+          /* Parent imports. Put .. last */
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+
+          /* Other relative imports. Put same-folder imports and . last */
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/extensions": [0],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-console": 0,
+    "no-control-regex": 0,
+
+    "prettier/prettier": ["error", { endOfLine: "auto", trailingComma: "all" }],
   },
 };
