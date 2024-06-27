5427e97262a234060381da4bdcca054b46f6c201
ImJustLucas <lucas.bellier@icloud.com>
2024-06-27 11:41:25 +0200

    🔒️ add jwt strategies

---
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { Request as RequestType } from "express";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  "jwt-refresh",
+) {
+  constructor(_configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        RefreshTokenStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: _configService.get<string>("JWT_REFRESHTOKEN_SECRET"),
+      expiresIn: _configService.get<number>("JWT_REFRESH_EXPIRATION_TIME"),
+      passReqToCallback: true,
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async validate(req: RequestType, payload: any) {
+    const refreshToken = req.cookies.refresh;
+    return { ...payload, refreshToken };
+  }
+
+  private static extractJWT(req: RequestType): string | null {
+    if (req.cookies && "refresh-token" in req.cookies) {
+      return req.cookies["refresh-token"];
+    }
+    return null;
+  }
+}
