af8e56fa9cf63c2af3928b0be4266f9ce1295596
ImJustLucas <lucas.bellier@icloud.com>
2024-07-18 23:28:03 +0200

    🚧 work on marketplace controller

--- src/marketplace/marketplace.controller.ts
+++ src/marketplace/marketplace.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import { MarketplaceService } from "./marketplace.service";
 
-@Controller('marketplace')
-export class MarketplaceController {}
+@Controller("marketplace")
+export class MarketplaceController {
+  constructor(private _marketplaceService: MarketplaceService) {}
+
+  @Get(":profileType")
+  async getBasicMarketplace(@Param("profileType") profileType: string) {
+    return this._marketplaceService.getBasicMarketplace(profileType);
+  }
+}
--- src/marketplace/marketplace.service.ts
+++ src/marketplace/marketplace.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { RidersService } from "src/riders/riders.service";
+import { SponsorsService } from "src/sponsors/sponsors.service";
 
 @Injectable()
-export class MarketplaceService {}
+export class MarketplaceService {
+  constructor(
+    private _ridersService: RidersService,
+    private _sponsorsService: SponsorsService,
+  ) {}
+
+  getBasicMarketplace(profileType: string) {
+    // let query;
+    // if (profileType === "rider") {
+    //   query = this._ridersService.findAll().sort({ createdAt: -1 });
+    // }
+    return `This is the basic marketplace for ${profileType}`;
+  }
+}
