fb1cee97c2ade9f49280e4db457ca81522491967
ImJustLucas <lucas.bellier@icloud.com>
2025-06-29 16:22:49 +0200

    🏷️ add createSavedSearchDto and corresponding type for saved searches in search.dtos.ts

--- src/features/search/types/search.dtos.ts
+++ src/features/search/types/search.dtos.ts
@@ -3,3 +3,11 @@ import { z } from "zod";
 export const SearchDto = z.object({
   search: z.string(),
 });
+
+export const createSavedSearchDto = z.object({
+  name: z.string(),
+  sponsorId: z.string(),
+  filters: z.record(z.string(), z.string()),
+});
+
+export type CreateSavedSearchDto = z.infer<typeof createSavedSearchDto>;
